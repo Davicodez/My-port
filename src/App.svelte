ac0bb6923423edac56edc2e55bcc396192a17e8b
Davicodez <112895705+Davicodez@users.noreply.github.com>
2022-10-21 17:12:35 -0700

    inti

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+ import NavBar from "./components/NavBar.svelte";
+ import About from "./components/About.svelte";
+</script>
+
+<!-- HTML -->
+<main>
+  <NavBar/>
+    <About/>
+    
+  
+  
+</main>
+
+<!-- CSS -->
+<style>
+
+</style>
---
+++ src/components/About.svelte
@@ -0,0 +1,33 @@
+<script>
+    import Bear from "../assets/Bear.svg"
+    
+
+</script>
+
+<section>
+    <div class="container_about" id="About">
+        <img width={180} height={123} src={Bear} alt=" "/>
+        <p class="header--small"> Hi, I'm bear</p>
+        <p class="header--big">WELCOME TO MY PORTFOLIO</p>
+    </div>
+</section>
+
+<style>
+    .container_about {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-top: 10rem;
+    }
+    .header--small {
+        font-size: 1.5rem;
+        margin: 2rem 0 0 0;
+        font-weight: 600;
+    }
+    .header--big {
+        font-size: 2rem;
+        color: #2C91C6;
+        font-weight: 700;
+
+    }
+</style>
